d38298fe9817f2fe7b901f88a8d27c5e46804792
Music Meier <musicm987@gmail.com>
2020-09-08 17:49:49 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,8 +4,8 @@ def load_library(path)
   final_hash = {
    YAML.load_library
    final_hash[key] = {}
-   final_hash[:english] = ""
-   final_hash[:japanese] = 
+   final_hash[:english] = value[0]
+   final_hash[:japanese] = value[1]
   }
   final_hash
 end
