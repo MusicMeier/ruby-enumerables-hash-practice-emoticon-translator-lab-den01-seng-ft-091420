312c0cb2f8515921c06638971c4a3b20cae60271
Music Meier <musicm987@gmail.com>
2020-09-08 17:53:14 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,13 +2,13 @@
 
 def load_library(path)
   final_hash = {}
-#   YAML.load_library(path).each do |key, value|
-#   final_hash[key] = {}
-#   final_hash[:english] = value[0]
-#   final_hash[:japanese] = value[1]
-# end
+  YAML.load_library(path).each do |key, value|
+  final_hash[key] = {}
+  final_hash[:english] = value[0]
+  final_hash[:japanese] = value[1]
+end
 
-#   final_hash
+  final_hash
 end
 
 def get_japanese_emoticon
